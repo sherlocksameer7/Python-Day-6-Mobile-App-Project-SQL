4bb16b0df231803e3668b700cd954da604f5add2
sherlocksameer7 <arijitsameers@gmail.com>
2022-03-09 21:32:07 +0530

    Changes has been made

--- Mobile_phone.py
+++ Mobile_phone.py
@@ -23,12 +23,22 @@ else:
 
 while True:
     print("Select an option from the Menu ? ")
+
     print("1. Add a Mobile phones ")
     print("2. Search a Mobile phones ")
     print("3. Update a Mobile phones ")
     print("4. Delete a Mobile phones ")
     print("5. View All Mobile phones ")
-    print("6. Exit ")
+    print("6. Max of all Mobile phones ")
+    print("7. Min of all Mobile phones ")
+    print("8. Avg of all Mobile phones ")
+    print("9. Count of all Mobile phones ")
+    print("10. Sum of all Mobile phones ")
+    print("11. Group of all Mobile phones ")
+    print("12. Order of all Mobile phones ")
+    print("13. Range Between of all Mobile phones ")
+    print("14. Max values and Details of all Mobile phones ")
+    print("15. Exit ")
 
     choice = int(input("Enter a Choice ? "))
 
@@ -114,6 +124,80 @@ while True:
         print(table)
 
     elif choice == 6:
+
+        result = connection.execute("Select Max(Price) as price From Smart_phones")
+
+        for i in result:
+            print("Maximum Price: ", i[0])
+
+    elif choice == 7:
+
+        result = connection.execute("Select Min(Price) as price From Smart_phones")
+
+        for i in result:
+            print("Minimum Price: ", i[0])
+
+    elif choice == 8:
+
+        result = connection.execute("Select Avg(Price) as price From Smart_phones")
+
+        for i in result:
+            print("Average Price: ", i[0])
+
+    elif choice == 9:
+
+        result = connection.execute("Select Count(*) as count From Smart_phones")
+
+        for i in result:
+            print("Total Count of an Mobile Phones: ", i[0])
+
+    elif choice == 10:
+
+        result = connection.execute("Select Sum(Price) as price From Smart_phones")
+
+        for i in result:
+            print("Sum of all Mobile Price: ", i[0])
+
+    elif choice == 11:
+
+        result = connection.execute("Select Brand_Name,Sum(Price) as price From Smart_phones Group by Brand_Name")
+        table = PrettyTable(["Brand", "Price"])
+
+        for i in result:
+            table.add_row([i[0], i[1]])
+        print(table)
+
+    elif choice == 12:
+
+        result = connection.execute("Select Brand_Name,Sum(Price) as price From Smart_phones Group by Brand_Name Order by Brand_Name Desc")
+        table = PrettyTable(["Brand", "Price"])
+
+        for i in result:
+            table.add_row([i[0], i[1]])
+        print(table)
+
+    elif choice == 13:
+
+        lower_Amount = input("Enter a lower number to print ? ")
+        Higher_Amount = input("Enter a higher number to print ? ")
+        result = connection.execute("Select * From Smart_phones Where Price Between "+lower_Amount+" And "+Higher_Amount)
+        table = PrettyTable(
+            ["ID", "Serial Number", "Brand Name", "Model Name", "Manufacturing Year", "Manufacturing Month", "Price"])
+
+        for i in result:
+            table.add_row([i[0], i[1], i[2], i[3], i[4], i[5], i[6]])
+        print(table)
+
+    elif choice == 14:
+
+        result = connection.execute("Select * From Smart_phones Where Price = (Select Max(Price) From Smart_phones)")
+        table = PrettyTable(["ID", "Serial Number", "Brand Name", "Model Name", "Manufacturing Year", "Manufacturing Month", "Price"])
+
+        for i in result:
+            table.add_row([i[0], i[1], i[2], i[3], i[4], i[5], i[6]])
+        print(table)
+
+    elif choice == 15:
         connection.close()
         break
 
